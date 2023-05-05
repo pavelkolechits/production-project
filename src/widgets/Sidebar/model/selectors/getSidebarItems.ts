52c496278572c708290c54f45d1410938f46e8bf
kolechits <pavelkolechits@gmail.com>
2023-05-05 05:35:59 +0300

    fix sidebar

---
+++ src/widgets/Sidebar/model/selectors/getSidebarItems.ts
@@ -0,0 +1,45 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getUserAuthData } from 'entities/User';
+import React from 'react';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import MainIcon from 'shared/assets/icons/mainIcon.svg';
+import AboutIcon from 'shared/assets/icons/aboutIcon.svg';
+import ProfileIcon from 'shared/assets/icons/profileIcon.svg';
+import ArticlesIcon from 'shared/assets/icons/articlesIcon.svg';
+import { SidebarItemType } from '../items';
+
+export const getSidebarItems = createSelector(
+    getUserAuthData,
+    (userData) => {
+        const SidebarItemsList: SidebarItemType[] = [
+            {
+                path: RoutePath.main,
+                Icon: MainIcon,
+                text: 'Главная',
+            },
+            {
+                path: RoutePath.about,
+                Icon: AboutIcon,
+                text: 'О сайте',
+            },
+        ];
+        if (userData) {
+            SidebarItemsList.push(
+                {
+                    path: RoutePath.profile + userData.id,
+                    Icon: ProfileIcon,
+                    text: 'Профиль',
+                    authOnly: true,
+                },
+                {
+                    path: RoutePath.articles,
+                    Icon: ArticlesIcon,
+                    text: 'Статьи',
+                    authOnly: true,
+                },
+            );
+        }
+        return SidebarItemsList;
+    },
+);
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,11 +1,13 @@
 import { classNames } from 'shared/lib/helpers/classNames/classNames';
-import { memo, useState } from 'react';
+import { memo, useMemo, useState } from 'react';
 import { ThemeSwither } from 'shared/ui/ThemeSwitcher';
 import { LanguageSwitcher } from 'shared/ui/LanguageSwither';
 import { Button, SizeButton, ThemeButton } from 'shared/ui/Button/Button';
 import { SidebarItemList } from 'widgets/Sidebar/model/items';
+import { useSelector } from 'react-redux';
 import cls from './Sidebar.module.scss';
 import { SidebarItem } from '../SidebarItem/SidebarItem';
+import { getSidebarItems } from '../../model/selectors/getSidebarItems';
 
 interface SidebarProps {
     className?: string;
@@ -13,9 +15,17 @@ interface SidebarProps {
 
 export const Sidebar = memo(({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const sidebarItemList = useSelector(getSidebarItems);
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
+    const itemsList = useMemo(() => sidebarItemList.map((item) => (
+        <SidebarItem
+            item={item}
+            collapsed={collapsed}
+            key={item.path}
+        />
+    )), [collapsed, sidebarItemList]);
     return (
         <div
             data-testid="sidebar"
@@ -34,9 +44,7 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
                 {collapsed ? '>' : '<'}
             </Button>
             <div className={cls.items}>
-
-                {SidebarItemList.map((item) => <SidebarItem key={item.path} collapsed={collapsed} item={item} />)}
-
+                {itemsList}
             </div>
             <div className={cls.switchers}>
                 <ThemeSwither />
